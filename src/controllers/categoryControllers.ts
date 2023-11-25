6f4bad8a38d96d7af9fbc86c7771696d00e19006
Afraa Alandijani <afraakandijani@gmail.com>
2023-11-25 15:10:38 +0300

    Format code

--- src/controllers/categoryControllers.ts
+++ src/controllers/categoryControllers.ts
@@ -1,110 +1,106 @@
-import { Request, NextFunction, Response } from "express"
-import slugify from "slugify"
+import { Request, NextFunction, Response } from 'express'
+import slugify from 'slugify'
 
-import { Category } from "../models/categorySchema"
-import { CategoryInput } from "../types"
+import { Category } from '../models/categorySchema'
+import { CategoryInput } from '../types'
 import { createHttpError } from '../util/createHTTPError'
 import {
-    createNewCategory,
-    deleteCategory,
-    findACtegories,
-    findACtegoryById,
-    findACtegoryBySlug,
-    updateCategoryById,
-    updateCategoryBySlug
-} from "../services/categoryService"
-
+  createNewCategory,
+  deleteCategory,
+  findACtegories,
+  findACtegoryById,
+  findACtegoryBySlug,
+  updateCategoryById,
+  updateCategoryBySlug,
+} from '../services/categoryService'
 
 export const getAllCategories = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const categories = await findACtegories()
-        res.status(200).json({
-            massege: 'Get All Categories ',
-            payload : categories
-   })
+  try {
+    const categories = await findACtegories()
+    res.status(200).json({
+      massege: 'Get All Categories ',
+      payload: categories,
+    })
   } catch (error) {
-      next(error)
+    next(error)
   }
 }
 export const createCategory = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { name } = req.body;
-        console.log(name)
-        await createNewCategory(name);
-        res.status(201).json({
-            message: 'The category has been created successfully',
-        });
-    } catch (error) {
-        next(error)
-    }
-};
+  try {
+    const { name } = req.body
+    console.log(name)
+    await createNewCategory(name)
+    res.status(201).json({
+      message: 'The category has been created successfully',
+    })
+  } catch (error) {
+    next(error)
+  }
+}
 
 export const getOneCategoryById = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = req.params.id;
-        const category = await findACtegoryById(id);
+  try {
+    const id = req.params.id
+    const category = await findACtegoryById(id)
 
-        res.status(200).json({
-            massege: 'Get Category',
-            payload: category
-        })
-    } catch (error) {
-        next(error)
-    }
-};
-export const getOneCategoryBySlug = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const slug =req.params.slug
-        const category = await findACtegoryBySlug(slug)
-        //console.log(category)
-        res.status(200).json({
-            massege: 'Get Category',
-            payload: category
-        })
-    } catch (error) {
-        next(error)
-    }
-};
-export const deleteOneCategory = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = req.params.id;
-        await deleteCategory(id);
-        res.status(200).json({
-            massege: 'The category has been deleted successfully',
-        })
-    } catch (error) {
-        next(error)
-    }
-};
-export const updateOneCategoryId = async (req: Request, res: Response, next: NextFunction) => { 
-    try {
-        const id = req.params.id;
-        const updatedCategory: CategoryInput = req.body;
-        const updated = await updateCategoryById(id, updatedCategory);
-        
-        res.status(200).json({
-            massege: 'The category has been updated successfully ',
-            payload:updated
-
-        });
-        
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).json({
+      massege: 'Get Category',
+      payload: category,
+    })
+  } catch (error) {
+    next(error)
+  }
 }
-export const updateOneCategoryBySulg = async (req: Request, res: Response, next: NextFunction) => { 
-    try {
-        const slug = req.params.slug
-        console.log(slug)
-        const updatedCategory: CategoryInput = req.body;
-        console.log(updatedCategory)
-        const updated = await updateCategoryBySlug(slug, updatedCategory); 
-        res.status(200).json({
-            massege: 'The category has been updated successfully ',
-             payload:updated  
-        });
-        
-    } catch (error) {
-        next(error);
-    }
+export const getOneCategoryBySlug = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const slug = req.params.slug
+    const category = await findACtegoryBySlug(slug)
+    //console.log(category)
+    res.status(200).json({
+      massege: 'Get Category',
+      payload: category,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+export const deleteOneCategory = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = req.params.id
+    await deleteCategory(id)
+    res.status(200).json({
+      massege: 'The category has been deleted successfully',
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+export const updateOneCategoryId = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = req.params.id
+    const updatedCategory: CategoryInput = req.body
+    const updated = await updateCategoryById(id, updatedCategory)
+
+    res.status(200).json({
+      massege: 'The category has been updated successfully ',
+      payload: updated,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+export const updateOneCategoryBySulg = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const slug = req.params.slug
+    console.log(slug)
+    const updatedCategory: CategoryInput = req.body
+    console.log(updatedCategory)
+    const updated = await updateCategoryBySlug(slug, updatedCategory)
+    res.status(200).json({
+      massege: 'The category has been updated successfully ',
+      payload: updated,
+    })
+  } catch (error) {
+    next(error)
+  }
 }
--- src/routers/categoryRoutes.ts
+++ src/routers/categoryRoutes.ts
@@ -1,20 +1,20 @@
 import express from 'express'
 
 import {
-    createCategory,
-    deleteOneCategory,
-    getAllCategories,
-    getOneCategoryById,
-    updateOneCategoryId,
-    getOneCategoryBySlug,
-    updateOneCategoryBySulg
+  createCategory,
+  deleteOneCategory,
+  getAllCategories,
+  getOneCategoryById,
+  updateOneCategoryId,
+  getOneCategoryBySlug,
+  updateOneCategoryBySulg,
 } from '../controllers/categoryControllers'
 const categoryRouter = express.Router()
 
-categoryRouter.get('/', getAllCategories);
+categoryRouter.get('/', getAllCategories)
 categoryRouter.get('/:slug', getOneCategoryBySlug)
 categoryRouter.get('/:id', getOneCategoryById)
-categoryRouter.post('/', createCategory);
+categoryRouter.post('/', createCategory)
 categoryRouter.delete('/:id', deleteOneCategory)
 categoryRouter.put('/:id', updateOneCategoryId)
 categoryRouter.put('/:slug', updateOneCategoryBySulg)
