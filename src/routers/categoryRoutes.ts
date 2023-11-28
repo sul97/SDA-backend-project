c7c8472c6e80aff01771ec5bb68d963cd379feec
Zahra Alsayed <145253041+ZahraAlsayed@users.noreply.github.com>
2023-11-28 18:04:56 +0300

    update vlidation function

--- src/routers/categoryRoutes.ts
+++ src/routers/categoryRoutes.ts
@@ -10,7 +10,7 @@ import {
   updateSingleCategoryBySulg,
   updateSingleategoryId
 } from '../controllers/categoryControllers'
-import { createCategoryValidation , updateCategoryValidation } from '../validation/categoryVlidation'
+import { createCategoryValidation  } from '../validation/categoryVlidation'
 import { runValidation } from '../validation/runValidation'
 
 const categoryRoutes = express.Router()
@@ -22,6 +22,6 @@ categoryRoutes.post('/',createCategoryValidation, runValidation,createCategory)
 categoryRoutes.delete('/:id', deleteSingleeCategoryById)
 categoryRoutes.delete('/:slug', deleteSingleCategoryBySlug)
 categoryRoutes.put('/:id', updateSingleategoryId)
-categoryRoutes.put('/:slug',updateCategoryValidation,runValidation, updateSingleCategoryBySulg)
+categoryRoutes.put('/:slug',createCategoryValidation,runValidation, updateSingleCategoryBySulg)
 
 export default categoryRoutes
