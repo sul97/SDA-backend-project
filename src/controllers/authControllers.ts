d9327cb98886d939c5683531e22bdb6222a44e49
Afraa Alandijani <afraakandijani@gmail.com>
2023-12-03 12:13:32 +0300

    backend_project_last

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,37 +1,15 @@
 import { Request, Response, NextFunction } from 'express'
-import bcrypt from 'bcrypt'
 
-import {User} from '../models/userSchema'
-import { createHttpError } from '../util/createHTTPError'
-import { generateJwtToken } from '../util/jwtTokenHelper'
-import { dev } from '../config'
-
+import { handleLoginService } from '../services/athServices'
 
 export const handleLogin = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { email, password } = req.body
-    const user = await User.findOne({ email: email })
-    if (!user) {
-      const error = createHttpError(404, 'User not found with this email')
-      throw error
-    }
-    const isPasswordMatch = await bcrypt.compare(password, user.password)
-    if (!isPasswordMatch) {
-      const error = createHttpError(404, 'Password did not match')
-      throw error
-    }
-    if (user.isBanned) {
-      const error = createHttpError(404, 'You are banned. Please contact the authority')
-      throw error
-    }
-   
-    const accessToken = generateJwtToken({ _id: user._id }, dev.app.jwtAccessKey, 
-      '7m',
-    )
+    const { accessToken } = await handleLoginService(email, password)
     res.cookie('access_token', accessToken, {
-        maxAge: 15 * 60 * 1000, // 15 mintues
-        httpOnly : true, // prevent from store in browser
-        sameSite:'none' // can be different port
+      maxAge: 15 * 60 * 1000, // 15 mintues
+      httpOnly: true, // prevent from store in browser
+      sameSite: 'none', // can be different port
     })
     res.status(200).send({
       message: 'user is logged in',
@@ -41,14 +19,13 @@ export const handleLogin = async (req: Request, res: Response, next: NextFunctio
   }
 }
 
-
 export const handleLogout = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      res.clearCookie('access_token')
-      res.status(200).send({
-        message: 'user is logged out',
-      })
-    } catch (error) {
-      next(error)
-    }
+  try {
+    res.clearCookie('access_token')
+    res.status(200).send({
+      message: 'user is logged out',
+    })
+  } catch (error) {
+    next(error)
   }
+}
